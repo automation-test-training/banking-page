52a71baccb67b520ea4f07167235f49a6c707192
Heaton Cai <heaton.cai@gmail.com>
2016-07-18 15:05:59 +0800

    seperate config and run codes

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,8 @@
 import menu from './menu/menu.module';
+import appConfig from './app.config';
+import appRun from './app.run';
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// the 2nd parameter is an array of 'requires'
-// 'starter.controllers' is found in controllers.js
 export default angular.module('banking', ['ionic', menu.name])
-
   .controller('PlaylistsCtrl', function($scope) {
     $scope.playlists = [
       { title: 'Reggae', id: 1 },
@@ -15,73 +13,8 @@ export default angular.module('banking', ['ionic', menu.name])
       { title: 'Cowbell', id: 6 }
     ];
   })
-
   .controller('PlaylistCtrl', function($scope, $stateParams) {
   })
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-  $stateProvider
-
-    .state('app', {
-    url: '/app',
-    abstract: true,
-    templateUrl: 'templates/menu.html',
-    controller: 'MenuCtrl'
-  })
-
-  .state('app.search', {
-    url: '/search',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/search.html'
-      }
-    }
-  })
-
-  .state('app.browse', {
-      url: '/browse',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/browse.html'
-        }
-      }
-    })
-    .state('app.playlists', {
-      url: '/playlists',
-      views: {
-        'menuContent': {
-          templateUrl: 'templates/playlists.html',
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-  .state('app.single', {
-    url: '/playlists/:playlistId',
-    views: {
-      'menuContent': {
-        templateUrl: 'templates/playlist.html',
-        controller: 'PlaylistCtrl'
-      }
-    }
-  });
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
-})
+  .run(appRun)
+  .config(appConfig)
   .constant('API_URL', 'http://localhost:8080');
---
+++ src/js/app.run.js
@@ -0,0 +1,15 @@
+/*@ngInject*/
+const AppRun = ($ionicPlatform) => {
+  $ionicPlatform.ready(() => {
+    if (window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+};
+
+export default AppRun;
