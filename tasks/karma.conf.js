490568dff3b56acf7d1282dbc1e4f92d2afe92ac
Heaton Cai <heaton.cai@gmail.com>
2016-09-08 16:44:56 +0800

    change report path for coverage

--- tasks/karma.conf.js
+++ tasks/karma.conf.js
@@ -57,7 +57,7 @@ module.exports = function (config) {
         type: 'text-summary',
       }, {
         type: 'html',
-        dir: 'reports/coverage/'
+        dir: '../reports/coverage/'
       }]
     },
 
